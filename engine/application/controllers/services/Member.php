93842664abfe1fdb61e59132831872715a2a6a31
Marwan Saleh <marwan@bsminsbroker.com>
2017-11-29 08:03:49 +0700

    Update member page

--- engine/application/controllers/services/Member.php
+++ engine/application/controllers/services/Member.php
@@ -209,7 +209,7 @@ class Member extends REST_Api {
         
         $anggota = $this->post('anggota');
         $event = $this->post('event');
-        $present = $this->post('event');
+        $present = $this->post('present');
         
         if ($this->rel_participant_m->get_count(array('anggota'=>$anggota,'event'=>$event))){
             $result['message'] = 'Peserta telah mengikuti event ini';
