23ebd40c1221a45d3f59c8f874225a896c00831f
Marwan Saleh <marwan@bsminsbroker.com>
2017-11-30 16:14:15 +0700

    New model migrasi event seat

---
+++ engine/application/models/Rel_interest_m.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of Rel_interest_m
+ *
+ * @author Marwan
+ * @email amazzura.biz@gmail.com
+ */
+class Rel_interest_m extends MY_Model {
+    protected $_table_name = 'rel_ketertarikan_anggota';
+    protected $_primary_key = 'id';
+    protected $_primary_filter = 'intval';
+    protected $_order_by = 'id';
+    protected $_timestamps = FALSE;
+}
+
+/*
+ * file location: /application/models/Rel_interest_m.php
+ */
---
+++ engine/application/models/Rel_pendidikan_m.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of Rel_pendidikan_m
+ *
+ * @author Marwan
+ * @email amazzura.biz@gmail.com
+ */
+class Rel_pendidikan_m extends MY_Model {
+    protected $_table_name = 'rel_pendidikan_anggota';
+    protected $_primary_key = 'id';
+    protected $_primary_filter = 'intval';
+    protected $_order_by = 'id';
+    protected $_timestamps = FALSE;
+}
+
+/*
+ * file location: /application/models/Rel_pendidikan_m.php
+ */
