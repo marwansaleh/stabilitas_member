09b4ae682e5f5042908f68f949ce8ab56861dd65
Marwan Saleh <marwan@bsminsbroker.com>
2017-12-04 09:28:35 +0700

    New model migrasi event seat

--- engine/application/views/components/_top_body.php
+++ engine/application/views/components/_top_body.php
@@ -4,7 +4,7 @@
         <div class="row">
             <!-- logo -->
             <div class="col-md-2 logo">
-                <a href="#"><img src="<?php echo get_asset_url('img/logo-stabilitas.png') ?>" alt="Admin Dashboard" height="20px" width="100px"></a>
+                <a href="#"><img class="hidden-xs" src="<?php echo get_asset_url('img/logo-stabilitas.png') ?>" alt="Admin Dashboard" height="20px" width="100px"></a>
                 <h1 class="sr-only">Stabilitas</h1>
             </div>
             <!-- end logo -->
@@ -15,16 +15,16 @@
                             <!-- responsive menu bar icon -->
                             <a href="#" class="hidden-md hidden-lg main-nav-toggle"><i class="fa fa-bars"></i></a>
                             <!-- container for status WS BROKOLI BRI -->
-                            <div class="btn-group">
+<!--                            <div class="btn-group">
                                 <a href="#" class="btn btn-link btn-status-ws" title="Cannot connect to BROKOLI BRI">
                                     <i class="fa fa-exclamation-triangle"></i>
                                 </a>
-                            </div>
+                            </div>-->
                             <!-- logged user and the menu -->
                             <div class="logged-user">
                                 <div class="btn-group">
                                     <a href="#" class="btn btn-link dropdown-toggle" data-toggle="dropdown">
-                                        <img class="avatar img-rounded" style="max-width: 28px; max-height: 30px;" src="<?php echo get_asset_url('img/avatar/'.$me->avatar) ?>" alt="User Avatar">
+                                        <img class="avatar img-rounded hidden-xs" style="max-width: 28px; max-height: 30px;" src="<?php echo get_asset_url('img/avatar/'.$me->avatar) ?>" alt="User Avatar">
                                         <span class="name"><?php echo $me->username; ?></span> <span class="caret"></span>
                                     </a>
                                     <ul class="dropdown-menu" role="menu">
