aaf520615b04ab8cc3e8e000431f8ea476c183c2
Marwan Saleh <marwan@bsminsbroker.com>
2017-12-04 14:29:19 +0700

    New model migrasi event seat

---
+++ engine/application/models/Ref_pendidikan_m.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of Ref_pendidikan_m
+ *
+ * @author Marwan
+ * @email amazzura.biz@gmail.com
+ */
+class Ref_pendidikan_m extends MY_Model {
+    protected $_table_name = 'ref_pendidikan';
+    protected $_primary_key = 'id';
+    protected $_primary_filter = 'intval';
+    protected $_order_by = 'id';
+    protected $_timestamps = FALSE;
+}
+
+/*
+ * file location: /application/models/Ref_pendidikan_m.php
+ */
